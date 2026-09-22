fcf2e05987593a98dbf47fe65b2fa01cea05bafa
Armin Afshar <armin4afshar@gmail.com>
2026-09-22 15:38:19 +0330

    Debug: Add Authorize to UserController

--- WebArMa.ArMaMelk.API.EndPoint/Controllers/UserController.cs
+++ WebArMa.ArMaMelk.API.EndPoint/Controllers/UserController.cs
@@ -1,11 +1,13 @@
 using Asp.Versioning;
 using Mediator;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using WebArMa.ArMaMelk.API.Application.Users.Commands.Update;
 using WebArMa.ArMaMelk.API.Application.Users.Queries.GetByGuid;
 
 namespace WebArMa.ArMaMelk.API.EndPoint.Controllers
 {
+    [Authorize]
     [ApiController]
     [ApiVersion(1.0)]
     [Route("api/v{version:apiVersion}/[controller]")]
